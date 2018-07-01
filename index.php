c8daeed86a5ca571206665f3f3863188ff038cb4
Tomohito Yamazaki <t07840ty@gmail.com>
2018-07-01 17:17:04 +0900

    "Initial commit for adding files"

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MITSUGO</title>
+</head>
+<body>
+<h2>Mitsugo Web Site</h2>
+</body>
+</html>
